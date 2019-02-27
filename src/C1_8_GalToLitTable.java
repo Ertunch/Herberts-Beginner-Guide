8b8d2e90d91de44563cd12603f9c00360f3e6976
Ertunc <ertuncaltintas@gmail.com>
2019-02-27 11:47:32 -0500

    Adding Gal To Lit Table

--- src/C1_8_GalToLitTable.java
+++ src/C1_8_GalToLitTable.java
@@ -1,9 +1,26 @@
-
+/*
+ * print a table of conversion up to 100
+ * Every 10 line, it will give blank line as well
+ * 
+ */
 public class C1_8_GalToLitTable {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		double gallons, liters;
+		int counter;
+		
+		counter = 0;
+		for (gallons = 1; gallons<=100; gallons++) {
+			liters = gallons * 3.7854;
+			System.out.println(gallons + " gallons is "+ liters + " liters.");
+			counter++;
+			//every 10th line print blank line
+			if (counter==10) {
+				System.out.println();
+				counter = 0;
+			}
+		}
 	}
 
 }
