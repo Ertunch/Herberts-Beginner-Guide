d1cb9a2617447268b950f623a43986536d6f52a9
Ertunc Altintas <eric@Ertuncs-MBP.fios-router.home>
2019-02-27 00:08:39 -0500

    Finishing first Example.java

--- src/Example.java
+++ src/Example.java
@@ -1,9 +1,31 @@
+//Name of the main class should be the same with the file name.
+//one line comment
+//to compile a file write "javac Example.java" then "java Example" on CLI
 
-public class Example {
+/*
+ Multi line comment 
+ */
+// Class is the basic unit of encapsulation.
+public // access modifier- how other parts of the program can access the members of the class
+
+class Example {// Members of the class will live inside this curly brackets.
+	//All programming activities happens within a class.
+	
 	//A Java program begins with a call to main ()
-
-	public static void main(String[] args) {
+	
+	public // it should be called outside of its class
+	static // allows main() to be called before an object of the class has been created
+	// main will be called by JVM before any objects are made
+	
+	void //main doesn't return a value.
+	main(String[] args)//creates args array String type 
+	
+	{//subroutine is called as a method
+		
+		//this program will start to run from main method.
+		
 		System.out.println("Java drives the web.");
 	}
 
 }
+//Java is case sensitive
