1d673116e32481f65180961f0ea4bfc8bb32b7ad
Ertunc <ertuncaltintas@gmail.com>
2019-02-28 11:03:29 -0500

    Adding Scope and lifetime of variables

---
+++ src/C2_6_LifetimeOfVariables.java
@@ -0,0 +1,18 @@
+/*
+ * This program attempts to declare a variable in an inner scope with the same name as one defined in an outer scope.
+ * This program will not compile.
+ */
+
+public class C2_6_LifetimeOfVariables {
+
+	public static void main(String[] args) {
+		int count; // method level scope, more global
+		
+		for (count = 0; count < 10; count++) {
+			int count; //illegal
+			
+		}
+		
+	}
+
+}
